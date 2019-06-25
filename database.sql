2eb9d2ef21a16bbbe9f8df096ecf90a7af536f78
Rasma Bayu <rasmabayu94@gmail.com>
2019-06-25 08:50:55 +0700

    db dummy

---
+++ database.sql
@@ -0,0 +1,12 @@
+-- create table user
+CREATE TABLE cockroach.user (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(60),
+	email VARCHAR(70)
+);
+
+-- dummy data table user
+INSERT INTO tugas_cockroach.user(name, email) VALUES
+('rasma', 'rasma@gmail.com'),
+('bayu', 'bayu@gmail.com'),
+('kisnandar', 'kisnandar@gmail.com'),

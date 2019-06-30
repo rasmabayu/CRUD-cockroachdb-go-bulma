4f420f3a2ccf2a96806548f1f82de92ff41e6c87
harry7naga <harry7naga@gmail.com>
2019-06-30 21:10:13 +0700

    tambah dummy data

--- database.sql
+++ database.sql
@@ -5,8 +5,10 @@ CREATE TABLE cockroach.user (
 	email VARCHAR(70)
 );
 
--- dummy data table user
+-- tambah dummy data untuk table user
 INSERT INTO tugas_cockroach.user(name, email) VALUES
 ('rasma', 'rasma@gmail.com'),
 ('bayu', 'bayu@gmail.com'),
-('kisnandar', 'kisnandar@gmail.com');
+('kisnandar', 'kisnandar@gmail.com'),
+('harriono', 'harriono@gmail.com'),
+('mahalludin', 'mahalludin@gmail.com');
